c4500a763ea2e374228bd4d24f0db2ae90c6c556
Krystian Postek <krystian@postek.eu>
2022-12-05 10:51:16 +0100

    Add function to parse stacks

--- supply-stacks/src/main.rs
+++ supply-stacks/src/main.rs
@@ -1,3 +1,28 @@
+struct SupplyStack {
+    stack: Vec<char>,
+}
+
+impl SupplyStack {
+    fn from_file() -> Vec<SupplyStack> {
+        let input = std::fs::read_to_string("stacks.txt").expect("Unable to find file!");
+        
+        let mut supplies = Vec::<SupplyStack>::new();
+
+        for line in input.lines() {
+            let mut stack = Vec::<char>::new();
+
+            for c in line.chars() {
+                stack.push(c);
+            }
+
+            supplies.push(SupplyStack { stack });
+        }
+
+        return supplies;
+    }
+}
+
 fn main() {
+    let _x = SupplyStack::from_file();
     println!("Hello, world!");
 }
