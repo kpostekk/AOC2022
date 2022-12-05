8e01e0ea41f8e0b45114a3e61fa2e218bade25e7
Krystian Postek <krystian@postek.eu>
2022-12-05 10:16:12 +0000

    Add regex

--- supply-stacks/src/main.rs
+++ supply-stacks/src/main.rs
@@ -1,3 +1,25 @@
+use regex::Regex;
+
+struct Stacks {
+    stacks: Vec<SupplyStack>,
+}
+
+impl Stacks {
+    fn create(stacks: Vec<SupplyStack>) -> Stacks {
+        return Stacks { stacks };
+    }
+
+    fn perform_command(&self, command: String) {
+        let count_regex = Regex::new(r"move (\d+)").unwrap();
+        let src_dst_regex = Regex::new(r"from (\d+) to (\d+)").unwrap();
+        
+        let count = count_regex.find(&command).expect("Unable to find move declaration").as_str();
+        let src = src_dst_regex.find(&command).expect("Unable to find from/todeclaration").as_str();
+        
+        print!("{}, {}", count, src)
+    }
+}
+
 struct SupplyStack {
     stack: Vec<char>,
 }
@@ -5,7 +27,7 @@ struct SupplyStack {
 impl SupplyStack {
     fn from_file() -> Vec<SupplyStack> {
         let input = std::fs::read_to_string("stacks.txt").expect("Unable to find file!");
-        
+
         let mut supplies = Vec::<SupplyStack>::new();
 
         for line in input.lines() {
@@ -23,6 +45,7 @@ impl SupplyStack {
 }
 
 fn main() {
-    let _x = SupplyStack::from_file();
+    let x = Stacks::create(SupplyStack::from_file());
+    x.perform_command("move 2 from 8 to 1".to_string());
     println!("Hello, world!");
 }
