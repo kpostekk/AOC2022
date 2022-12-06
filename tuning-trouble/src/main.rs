27933e76a48197d438e8453990c61d1690769dde
Krystian Postek <krystian.postek@ledatel.pl>
2022-12-06 11:35:00 +0100

    Make day 6 solution cleaner

--- tuning-trouble/src/main.rs
+++ tuning-trouble/src/main.rs
@@ -4,31 +4,19 @@ fn load_input() -> String {
     return std::fs::read_to_string("input.txt").expect("File does not exists!");
 }
 
-fn find_start_seq(input: &String) -> u32 {
-    for i in 0..(input.len() - 4) {
-        let seq = input.chars().skip(i).take(4).collect::<HashSet<char>>();
-        if seq.len() == 4 {
-            return (i + 4) as u32;
+fn find_unique_seq(input: &String, seq_size: usize) -> Option<usize> {
+    for i in 0..(input.len() - seq_size) {
+        let seq = input.chars().skip(i).take(seq_size).collect::<HashSet<char>>();
+        if seq.len() == seq_size {
+            return Some(i + seq_size);
         }
     }
-
-    panic!("Sequence does not have unique 4 element subsequence!")
-}
-
-fn find_message_seq(input: &String) -> u32 {
-    for i in 0..(input.len() - 14) {
-        let seq = input.chars().skip(i).take(14).collect::<HashSet<char>>();
-        if seq.len() == 14 {
-            return (i + 14) as u32;
-        }
-    }
-
-    panic!("Sequence does not have unique 4 element subsequence!")
+    return None;
 }
 
 fn main() {
     let input = load_input();
-    let start_marker = find_start_seq(&input);
-    let end_marker = find_message_seq(&input);
+    let start_marker = find_unique_seq(&input, 4).expect("Unable to find subsequence!");
+    let end_marker = find_unique_seq(&input, 14).expect("Unable to find subsequence!");
     println!("{}, {}", start_marker, end_marker);
 }
